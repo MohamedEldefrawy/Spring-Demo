b4a458d5399cb05397028517e66abfd0cfeea545
Mohamed  Mohamed Ahmed Aly Eldafrawy <meldafrawy@ISFPDOMAIN.com>
2022-08-03 17:43:25 +0200

    prepair hibernate session

--- src/main/java/com/example/springdemo/SpringDemoApplication.java
+++ src/main/java/com/example/springdemo/SpringDemoApplication.java
@@ -1,7 +1,8 @@
 package com.example.springdemo;
 
-import com.example.springdemo.contract.Shape;
+import com.example.springdemo.dto.UserDetails;
 import com.example.springdemo.service.ShapeService;
+import org.hibernate.Transaction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -11,9 +12,15 @@ public class SpringDemoApplication {
 
     public static void main(String[] args) {
         var context = new ClassPathXmlApplicationContext("spring.xml");
-//        context.registerShutdownHook();
-//        Shape shape = (Shape) context.getBean("triangle");
+        Transaction transaction = null;
+
         var shape = context.getBean("shapeService", ShapeService.class);
+        UserDetails userDetails = new UserDetails();
+        userDetails.setUserId(1);
+        userDetails.setName("Mohamed");
+
+//        Create hibernate session factory
+
 
 //        shape.draw();
         System.out.println(shape.getCircle().getCenter().getX());
